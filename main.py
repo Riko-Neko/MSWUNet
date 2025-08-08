1666a47222a1b44cc5b5c99e4b42c2b1a012b80b
RikoNeko <rikoneko@RikoNekodeMacBook-Air.local>
2025-08-09 00:17:20 +0800

    Modify bugs

--- main.py
+++ main.py
@@ -432,7 +432,7 @@ def main():
     valid_steps = 30
 
     # Loss function and optimizer
-    criterion = CombinedLoss(device, alpha=0.5, beta=0.5, delta=0.2, momentum=0.99)
+    criterion = CombinedLoss(device, alpha=0.5, beta=0.5, gamma=0.1, delta=0.2, momentum=0.99)
     optimizer = optim.Adam(list(model.parameters()) + list(criterion.parameters()), lr=5e-4, weight_decay=1e-7)
     # scheduler = optim.lr_scheduler.ReduceLROnPlateau(
     #     optimizer,
